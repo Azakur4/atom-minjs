70a52e845b66004c9d8c474964b03e8ad3e1a9c7
Manan Shah <manan.shah.777@gmail.com>
2014-09-11 20:51:53 -0700

    update tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
-console.log('this is a test')
-console.log('should minify this?')
-console.log('hello world')
+var downloadURL = function downloadURL(url) {
+    var hiddenIFrameID = 'hiddenDownloader',
+        iframe = document.getElementById(hiddenIFrameID);
+    if (iframe === null) {
+        iframe = document.createElement('iframe');
+        iframe.id = hiddenIFrameID;
+        iframe.style.display = 'none';
+        document.body.appendChild(iframe);
+    }
+    iframe.src = url;
+};
